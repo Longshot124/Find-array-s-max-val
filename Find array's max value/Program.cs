519ee1d1fea864f2ae93dee3867e9c6caec8e93e
kamra <kamra@LAPTOP-M49P7DNF>
2022-04-20 12:44:41 +0400

    first commit

---
+++ Find array's max value/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Find_array_s_max_value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {int[] arr = { 5,6,13,15,22,3,45,11,};
+
+            int maxVal = arr.Max();
+            Console.WriteLine(maxVal);
+        }
+    }
+}
